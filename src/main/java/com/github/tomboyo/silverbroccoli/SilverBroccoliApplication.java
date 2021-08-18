92d6e32418da74c302c2d87067892e8c25bed96c
Tom Simmons <thsimmon@redhat.com>
2021-08-18 16:59:00 -0400

    Define number supplier and consumer

--- src/main/java/com/github/tomboyo/silverbroccoli/SilverBroccoliApplication.java
+++ src/main/java/com/github/tomboyo/silverbroccoli/SilverBroccoliApplication.java
@@ -1,11 +1,35 @@
 package com.github.tomboyo.silverbroccoli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SilverBroccoliApplication {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SilverBroccoliApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(SilverBroccoliApplication.class, args);
+  }
+
+  @Bean
+  public Supplier<String> numberSupplier() {
+    var counter = new AtomicInteger();
+    return () -> {
+      var n = counter.getAndIncrement();
+      return String.valueOf(n);
+    };
+  }
+
+  @Bean
+  public Consumer<String> printer() {
+    return it -> LOGGER.info("printer - " + it);
   }
 }
