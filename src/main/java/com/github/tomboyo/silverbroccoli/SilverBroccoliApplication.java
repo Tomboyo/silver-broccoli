b647c8e554c14c6f83d4c40fb0375a1ef5cc5dd0
Tom Simmons <thsimmon@redhat.com>
2021-08-19 15:29:31 -0400

    Support SASL/PLAIN via openshift profile

--- src/main/java/com/github/tomboyo/silverbroccoli/SilverBroccoliApplication.java
+++ src/main/java/com/github/tomboyo/silverbroccoli/SilverBroccoliApplication.java
@@ -5,10 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+
+import static org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer.ControlFlag.REQUIRED;
 
 @SpringBootApplication
 public class SilverBroccoliApplication {
@@ -31,5 +38,20 @@ public class SilverBroccoliApplication {
   @Bean
   public Consumer<String> printer() {
     return it -> LOGGER.info("printer - " + it);
+  }
+
+  @Bean
+  @Profile("openshift")
+  public KafkaJaasLoginModuleInitializer jaasConfig(Environment env) throws IOException {
+    var module = new KafkaJaasLoginModuleInitializer();
+    module.setLoginModule("org.apache.kafka.common.security.plain.PlainLoginModule");
+    module.setControlFlag(REQUIRED);
+    module.setOptions(
+        Map.of(
+            "username",
+            env.getRequiredProperty("env.kafka.username"),
+            "password",
+            env.getRequiredProperty("env.kafka.password")));
+    return module;
   }
 }
