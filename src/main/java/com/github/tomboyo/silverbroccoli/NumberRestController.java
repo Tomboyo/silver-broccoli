18003ca5402b305c2d85d97afdbdffab909ea1d5
Tom Simmons <thsimmon@redhat.com>
2021-08-22 15:54:59 -0400

    Use localhost docker. Attach number handlers to profile.

--- src/main/java/com/github/tomboyo/silverbroccoli/NumberRestController.java
+++ src/main/java/com/github/tomboyo/silverbroccoli/NumberRestController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/number")
+@Profile("numbers")
 public class NumberRestController {
 
   public static final class EnqueueBody {
--- src/main/java/com/github/tomboyo/silverbroccoli/SilverBroccoliApplication.java
+++ src/main/java/com/github/tomboyo/silverbroccoli/SilverBroccoliApplication.java
@@ -8,15 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static org.springframework.kafka.security.jaas.KafkaJaasLoginModuleInitializer.ControlFlag.REQUIRED;
 
@@ -27,49 +21,6 @@ public class SilverBroccoliApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SilverBroccoliApplication.class, args);
-  }
-
-  // tag::generate-message-with-supplier[]
-  @Bean
-  public Supplier<Integer> producer() {
-    var counter = new AtomicInteger();
-    return () -> {
-      var n = counter.getAndIncrement();
-      LOGGER.info("Producing number: " + n);
-      return n;
-    };
-  }
-  // end::generate-message-with-supplier[]
-
-  // tag::transform-message-with-function[]
-  @Bean
-  public Function<Integer, NumberMessage> transformer() {
-    return it -> {
-      LOGGER.info("Structuring number: " + it);
-      return new NumberMessage(it);
-    };
-  }
-  // end::transform-message-with-function[]
-
-  // tag::consume-message-with-consumer[]
-  @Bean
-  public Consumer<NumberMessage> consumer() {
-    return it -> LOGGER.info("Consuming number: " + it);
-  }
-  // end::consume-message-with-consumer[]
-
-  @Bean
-  public Consumer<Message<NumberMessage>> debugConsumer() {
-    return message -> {
-      var headers = message.getHeaders();
-      var partition = headers.get(KafkaHeaders.RECEIVED_PARTITION_ID);
-      var consumerGroup = headers.get(KafkaHeaders.GROUP_ID);
-      var payload = message.getPayload();
-      LOGGER.info(
-          String.format(
-              "%nPartition=%s%nThread=%s%nConsumerGroup=%s%nPayload=%s",
-              partition, Thread.currentThread().getName(), consumerGroup, payload));
-    };
   }
 
   @Bean
