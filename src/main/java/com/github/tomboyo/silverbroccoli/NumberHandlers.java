684ad3c37ff88e8e34c13f5f913c984871886222
Tom Simmons <thsimmon@redhat.com>
2021-08-23 12:23:31 -0400

    Add health and bindings actuator documentation

--- src/main/java/com/github/tomboyo/silverbroccoli/NumberHandlers.java
+++ src/main/java/com/github/tomboyo/silverbroccoli/NumberHandlers.java
@@ -2,7 +2,6 @@ package com.github.tomboyo.silverbroccoli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
