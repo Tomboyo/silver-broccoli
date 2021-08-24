ecbe9613e0c426eea25bbe26437fc512dee41eba
Tom Simmons <thsimmon@redhat.com>
2021-08-24 17:12:09 -0400

    Add idempotence, EOS, transacted examples

---
+++ src/main/java/com/github/tomboyo/silverbroccoli/txn/AuditLogRepository.java
@@ -0,0 +1,24 @@
+package com.github.tomboyo.silverbroccoli.txn;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface AuditLogRepository extends CrudRepository<AuditLog, Long> {
+
+  /**
+   * Create a new audit log entry if and only if another with the same message does not already
+   * exist. This is atomic and idempotent.
+   *
+   * @param message
+   */
+  @Transactional
+  @Modifying
+  @Query(
+      nativeQuery = true,
+      value = "insert into audit_log (message) values (?1) on conflict (message) do nothing")
+  void createIfNotExists(String message);
+}
